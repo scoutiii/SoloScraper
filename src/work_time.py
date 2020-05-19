0b642f5f9d402418df504465af30d90e49b31a8c
scoutiii <33967844+scoutiii@users.noreply.github.com>
2020-05-18 20:27:12 -0600

    Just made some updates and tweeks, putting it through a large test to see how it goes. After that I'll be starting to try and classify jobs better.

--- src/work_time.py
+++ src/work_time.py
@@ -9,25 +9,32 @@ from tqdm import tqdm
 
 # Class which has all characteristics of a message
 class message_info:
-
 	def __init__(self, message, customer_id):
-		self.msg_full = re.sub('[^a-zA-Z0-9 `~!@#$%^&*()_+-=\[\{\]\}\\\|\;\:\'\"\,\<\.\>\/\?\n]*', "", message)
+		self.msg_full = re.sub('[^a-zA-Z0-9 \`\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\{\]\}\\\|\;\:\'\"\,\<\.\>\/\?\\n]*', "", message)
 		self.cid = customer_id
-		reg_res = re.match("([a-zA-Z0-9 ]*) \(([a-zA-Z0-9 ]*)\) - ([a-zA-Z0-9/ :]*)\\n([^a-zA-Z0-9 `~!@#$%^&*()_+-=\[\{\]\}\\\|\;\:\'\"\,\<\.\>\/\?\n]*)", self.msg_full)
+		reg_res = re.match("([a-zA-Z0-9 -]*) \(([a-zA-Z0-9 ]*)\) - ([a-zA-Z0-9/ :]*)\\n([a-zA-Z0-9 \`\~\!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\{\]\}\\\|\;\:\'\"\,\<\.\>\/\?\\n]*)", self.msg_full)
 		if reg_res is not None:
 			self.name = reg_res.group(1)
 			self.title = reg_res.group(2)
 			self.date = datetime.strptime(reg_res.group(3), "%m/%d/%Y %H:%M")
 			self.msg = reg_res.group(4)
+			self.type = "Other"
+			self.sub_type = "Other"
 		else:
 			self.name = "NA"
 			self.title = "NA"
 			self.date = "NA"
 			self.msg = "NA"
-		#print(self.name + " " + self.title + " " + str(self.date))
+			self.type = "NA"
+			self.sub_type = "NA"
 
 
-
+# Class which classifies messages, and determins timings
+class message_timings:
+	def __init__(self, messages, customer_id):
+		self.messages = []
+		for msg in messages:
+			self.messages.append(message_info(msg, customer_id))
 
 
 # Takes a customer id and returns a list of every message
@@ -39,6 +46,7 @@ def get_messages(driver, customer_id):
 		messages.append(msg.text)
 	return(messages)
 
+
 # Is called first, will return a list of entries to put into the csv
 def create_entries(driver, customer_id):
 	messages = get_messages(driver, customer_id)
@@ -48,14 +56,7 @@ def create_entries(driver, customer_id):
 		entry = {"ID":msg_info.cid, "NAME":msg_info.name, "TITLE":msg_info.title,
 				 "DATE":msg_info.date, "MSG":msg_info.msg}
 		messages_info.append(entry)
-		#print(str(len(msg_info.msg)) + " : " + str(msg_info.cid))
-		#sys.stdout.flush()
 	return(messages_info)
-
-
-
-
-
 
 
 
@@ -65,20 +66,20 @@ def run(driver, file_in, file_out):
 	sys.stdout.flush()
 
 	# opens files and csv files
-	f_in = open(file_in, "r")
+	f_in = open(file_in, "r", encoding="utf8")
 	f_ut = open(file_out, "w")
 	csv_in = csv.DictReader(f_in)
 	csv_ut = csv.DictWriter(f_ut, ["ID","NAME", "TITLE", "DATE", "MSG"])
 	csv_ut.writeheader()
 
 	# Gets all ids first, so that we can have a progress bar
-	customer_ids = []
+	customer_ids = set()
 	for line in csv_in:
-		customer_ids.append(line)
+		customer_ids.add(line["customer_id"])
 
 	# loops through all customer ids, and writes the csv entries
 	for customer_id in tqdm(customer_ids):
-		entries = create_entries(driver, customer_id["customer_id"])
+		entries = create_entries(driver, customer_id)
 		csv_ut.writerows(entries)
 
 	f_in.close()
